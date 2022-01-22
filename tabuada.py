f8180364fc82de72325567bcda74d21ef16e1a86
joaopribeiro92 <98223842+joaopribeiro92@users.noreply.github.com>
2022-01-22 16:34:56 -0400

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,18 @@
+# Tabuada do 2
+
+#2 x 1 = 2
+#2 x 2 = 4
+#2 x 3 = 6
+#2 x 4 = 8
+#2 x 5 = 10
+#2 x 6 = 12
+#2 x 7 = 14
+#2 x 8 = 16
+#2 x 9 = 18
+
+tabuada = list(range(1,10))
+#print(tabuada)
+
+for element in tabuada:
+    result = element * 2
+    print('2 x {} = {}'.format(element, result))
